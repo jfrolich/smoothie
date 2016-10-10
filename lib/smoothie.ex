fa00555677bacf52388a5b3799234231af970bb8
Stephen Moloney <stephenmoloney@live.com>
2016-10-11 00:21:49 +0100

    make changes to allow for multiple smoothie configurations

--- lib/smoothie.ex
+++ lib/smoothie.ex
@@ -1,71 +1,63 @@
 defmodule Smoothie do
   require EEx
 
-  defmacro __using__(_options) do
-    quote do
-      import unquote(__MODULE__)
+  defmacro __using__(opts) do
 
-      generate_views()
-    end
-  end
+    quote bind_quoted: [opts: opts] do
 
-  # location of the template path
-  @template_path [Mix.Project.build_path, '..', '..'] ++ [Application.get_env(:smoothie, :template_dir)]
+      otp_app = Keyword.fetch!(opts, :otp_app)
+      smoothie_config = Keyword.fetch!(opts, :config)
+      template_path = [Mix.Project.build_path, '..', '..'] ++ [Application.get_env(otp_app, smoothie_config)[:template_dir]]
+      build_path = template_path ++ ["build"]
+      template_files = File.ls!(Path.join(build_path))
 
-  # location of the build path
-  @build_path @template_path ++ ["build"]
-
-  # create the template and build folder at compile time if not exists
-  unless File.exists?(Path.join(@build_path)), do: File.mkdir_p!(Path.join(@build_path))
-
-  @template_files File.ls!(Path.join(@build_path))
-  |> Enum.filter(fn(file) -> String.contains?(file, ".eex") end)
-
-  # Ensure the macro is recompiled when the templates are changed
-  @template_files
-  |> Enum.each(fn(file) ->
-    @external_resource Path.join(@build_path ++ [file])
-  end)
-
-  defmacro generate_views do
-    @template_files
-    |> Enum.map(fn(file) ->
-      # read the contents of the template
-      template_contents = File.read!(Path.join(@build_path ++ [file]))
-
-      # capture variables that are defined in the template
-      variables =
-        Regex.scan(~r/<%=[^\w]*(\w+)[^\w]*%>/, template_contents)
-        |> Enum.map(fn(match) ->
-          match
-          |> Enum.at(1)
-          |> String.to_atom
-        end)
-        |> Enum.uniq
-
-      # create assignment macro code for in the function block
-      variable_assignments = variables |> Enum.map(fn(name) ->
-        quote do
-          unquote(Macro.var(name, nil)) = args[unquote(name)]
-        end
+      # Ensure the macro is recompiled when the templates are changed
+      Enum.each(template_files, fn(file) ->
+        external_resource = Path.join(build_path ++ [file])
       end)
 
-      # generate function name from file name
-      template_name =
-        file
-        |> String.replace(".eex", "")
-        |> String.replace(".html", "_html")
-        |> String.replace(".txt", "_text")
-        |> String.to_atom
+      Enum.each(template_files, fn(file) ->
+        # read the contents of the template
+        template_contents = File.read!(Path.join(build_path ++ [file]))
 
-      compiled = EEx.compile_string(template_contents, [])
+        # capture variables that are defined in the template
+        variables =
+          Regex.scan(~r/<%=(.*?)%>/, template_contents)
+          |> Enum.map(fn(match) ->
+            match
+            |> Enum.at(1)
+            |> String.trim(" ")
+            |> String.to_atom()
+          end)
+          |> Enum.uniq
 
-      quote do
+        # create assignment macro code for in the function block
+        variable_assignments = quote do: (unquote(variables)
+        |> Enum.map(fn(name) ->
+          quote do
+            unquote(Macro.var(name, nil)) = args[unquote(name)]
+          end
+        end)
+        )
+
+        # generate function name from file name
+        template_name =
+          file
+          |> String.replace(".eex", "")
+          |> String.replace(".html", "_html")
+          |> String.replace(".txt", "_text")
+          |> String.to_atom
+
+        compiled = quote do: EEx.compile_string(unquote(template_contents), [])
+
         def unquote(template_name)(args) do
           unquote(variable_assignments)
           unquote(compiled)
         end
-      end
-    end)
+      end)
+
+    end
   end
+
+
 end
