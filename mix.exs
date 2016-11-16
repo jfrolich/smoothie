c152472774d77cb4b263e914373b2923fd66642e
Jaap Frolich <jaap@she-corp.com>
2016-11-16 19:25:19 +0800

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Smoothie.Mixfile do
   def project do
     [
       app: :smoothie,
-      version: "2.0.1",
+      version: "2.0.2",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
