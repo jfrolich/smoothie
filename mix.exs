5f976fb16ce1970e630a3e5a63a419bd61e392f5
Jaap Frolich <jfrolich@gmail.com>
2017-02-21 11:31:51 +0800

    create major release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Smoothie.Mixfile do
   def project do
     [
       app: :smoothie,
-      version: "2.0.1",
+      version: "3.0.0",
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
